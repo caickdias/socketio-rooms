34041fb4a6a578a5d7ae4b151c37313fc2ae3ac8
caickdias <caickandrade0@gmail.com>
2022-11-02 00:35:50 -0300

    added basic homepage layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,44 @@ import './App.css';
 function App() {
 
   return (
-    <div>
-        opa
+    <div className="container" style={styles.container}>
+        <div style={styles.appTitleCard}>
+            <h1>ROOMS</h1>
+        </div>
+
+        <div style={styles.lineDivider}></div>
+
+        <div style={styles.roomsContainer}>
+
+        </div>
     </div>
   );
+}
+
+const styles = {
+    container: {
+        display: 'flex',        
+        flexDirection: 'column',
+        backgroundColor: '#ecf0f1',        
+    },
+    appTitleCard: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: '#3498db',           
+        height: 69,
+        width: '100%',
+        borderRadius: 10,
+    },
+    lineDivider: {
+        width: '100%',
+        height: 1,
+        backgroundColor: 'gray',
+    },
+    roomsContainer: {
+        display: 'flex',
+        flex: 1,        
+    } 
 }
 
 export default App;
